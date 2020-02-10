8ef813217d0d22eb5881b0e3bf939863f7f39c62
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-02-10 18:55:31 -0500

    Create Classroom handler for PUT request

--- Classroom.js
+++ Classroom.js
@@ -1,9 +1,15 @@
 class Classroom {
-   constructor() {
-
-   }
-
-   set joinCode() {
-       this._joinCode = Math.random().toString(36).substr(2, 8);
-   }
+    constructor(_collection) {
+        this._collection = _collection;        
+    }
+    
+    handle_PUT(request, response) {
+        request.body["joinCode"] = Math.random().toString(36).substr(2, 8);
+        this._collection.insertOne(request.body, (error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            } 
+            response.status(200).send(result.result);
+        })
+    }
 }
