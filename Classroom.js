688aea52a098d6c9a712ede16a31a6843b466d9d
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-02-11 21:14:00 -0500

    Use shortid package to generate join code

--- Classroom.js
+++ Classroom.js
@@ -1,10 +1,11 @@
+const shortid = require('shortid');
 class Classroom {
     constructor(_collection) {
         this._collection = _collection;        
     }
     
     handle_PUT(request, response) {
-        request.body["joinCode"] = Math.random().toString(36).substr(2, 8);
+        request.body["joinCode"] = shortid.generate();
         this._collection.insertOne(request.body, (error, result) => {
             if(error) {
                 return response.status(500).send(error);
