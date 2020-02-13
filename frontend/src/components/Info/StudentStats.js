8d97318e768f45c2d01ccdb304f039680fa5b801
Micah Flemming <micah.flemming@utoronto.ca>
2020-02-12 20:44:39 -0500

    [AUT-22] Add student info to the StudentStats.js page

--- frontend/src/components/Info/StudentStats.js
+++ frontend/src/components/Info/StudentStats.js
@@ -5,7 +5,7 @@ class StudentStats extends React.Component {
 
    constructor(props) {
       super(props)       
-	   this.state = { studentName: "Student's Name", courseName: "CSC301 - Software Engineering", grades: [ { id: 1, title: 'Quiz 1', mark: 5, max_mark: 10 }, { id: 2, title: 'Quiz 2', mark: 5, max_mark: 10 }, { id: 3, title: 'Assignment 1', mark: 15, max_mark: 20 }]
+	   this.state = { studentId: "Student ID", studentName: "Student Name", courseName: "CSC301 - Software Engineering", grades: [ { id: 1, title: 'Quiz 1', mark: 5, max_mark: 10 }, { id: 2, title: 'Quiz 2', mark: 5, max_mark: 10 }, { id: 3, title: 'Assignment 1', mark: 15, max_mark: 20 }]
       }
    }
 
@@ -27,16 +27,19 @@ class StudentStats extends React.Component {
       return (
          <div>
             <h1 id='title'> { this.state.courseName } </h1>
-            <table id='students' align='center'>
+            <table id='students'>
                <tbody>
-	          <tr> <b>
-	      	     <td> Type of Assesment </td>
-	      	     <td> Grade </td>
-	      	     <td> Max Grade </td>
-	          </b> </tr>
+	          <tr> 
+	          <b>
+	      	     <td>Type of Assesment</td>
+	      	     <td>Grade</td>
+	      	     <td>Max Grade</td>
+	          </b> 
+	          </tr>
                   { this.renderTable() }
                </tbody>
             </table>
+	    <h5> {this.state.studentName} | {this.state.studentId} </h5>
          </div>
       )
    } 
