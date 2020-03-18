06d6ac3549be1cc6fb2c419c978648fda1868510
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-03-18 13:42:12 -0400

    Create handler for PUT request for a classroom

--- backend/api/controllers/classroomController.js
+++ backend/api/controllers/classroomController.js
@@ -1,4 +1,34 @@
 const mongoose = require("mongoose");
+const Classroom = require("../models/schemas/classroomSchema")
 
 
-/* classroom related db queries go here */
+//function handle_GET(req, res, next) {
+
+//}
+
+function handle_PUT(req, res, next) {
+    const classroom = new Classroom({
+        _id: new mongoose.Types.ObjectId(),
+        course_code: req.body.courseCode,
+        name: req.body.name,
+        instructor_ids: req.body.instructor_ids,
+        student_ids: req.body.student_ids,
+        quizlets: req.body.quizlets,
+        marks: req.body.marks
+    });
+    classroom.save()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: "Handling PUT request to /classroomController",
+                createdClassroom: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
--- backend/api/models/schemas/classroomSchema.js
+++ backend/api/models/schemas/classroomSchema.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const classroomSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
-	course_code: {type: String, required: true, unique: true },
+	course_code: { type: String, required: true, unique: true },
 	name: { type: String, required: true },
-    instructorIds: { type: Array, required: true },
-    studentIds: { type: Array, required: true },
-	quizlets: {type: Array}
+    instructor_ids: { type: Array, required: true },
+    student_ids: { type: Array, required: true },
+	quizlets: { type: Array },
+	marks: [{
+		student_id: { type: String, required: true, unique: true },
+		mark: { type: Number, required: true, unique: true }
+	}]
 });
 
 
