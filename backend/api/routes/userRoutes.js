b6cd7861fc33402d5e40d0e61d2f5bb9eaf531d1
Kyle Jang <kyle.jang@mail.utoronto.ca>
2020-03-22 17:07:53 -0400

    Create routes for classroom and user

--- backend/api/routes/userRoutes.js
+++ backend/api/routes/userRoutes.js
@@ -5,6 +5,22 @@ var mongoose    = require("mongoose");
 
 var User = require('../models/schemas/userSchema');
 
+router.get('/:user_id', (req, res) => {
+    const id = req.params.user_id;
+    User.findById(id)
+        .exec()
+        .then(doc => {
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({message: 'No valid entry found'});
+            };       
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+})
 
 router.post('/signup', (req, res) => {
     // console.log(req.body);
