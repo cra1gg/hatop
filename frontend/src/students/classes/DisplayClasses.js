e94f2383632ee8c79d86242398d463c1d4ba3e06
Shubham <shar.shub@outlook.com>
2020-03-26 02:43:47 -0400

    [Hotfix] Fix the ugly background image for classes

--- frontend/src/students/classes/DisplayClasses.js
+++ frontend/src/students/classes/DisplayClasses.js
@@ -5,23 +5,26 @@ const ClassList = ({classes}) => {
   const classList = classes.length ? (
     classes.map(cl => {
       return (
-        <div className="container">
-            <div className="col s12 m5 cards-container" key={cl.id}>
-                <div className="card blue-grey darken-2">
+            <div className="col s6 16" key={cl.id}>
+                <div className="card blue-grey darken-10">
                     <div className="card-image">
-                        <img src={require("../images/background.jpeg")} alt="" width="300" height="300"/>
+                        <img src={require("../images/innovation-brain.jpg")} alt={cl.course_code} />
                     </div>
                     <div className="card-content white-text">
                         <span className="card-title">{cl.courseCode}</span>
 
-                        <span>{cl.desc}</span>
+                        <span>{cl.desc}  *insert description about the course* Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta temporibus suscipit, ipsam pariatur esse numquam nobis expedita odio, dolores, accusamus doloremque exercitationem ut eum laborum officia maxime labore similique distinctio?</span>
                     </div>
                     <div className="card-action">
-                        <a href="/">View</a>
+                    {/* Each route makes a GET request for the grades and quizzes given the classcode + username in the get request. */}
+                        <a href="/viewClasses">View grades</a>   
+                        <a href="/viewClasses">View quizzes</a>
                     </div>
                 </div>
             </div>
-        </div>
+
+
+
 
       )
     })
@@ -30,8 +33,13 @@ const ClassList = ({classes}) => {
   );
 
   return (
-    <div className="class collection">
-      {classList}
+    <div className="class collection grey lighten-5">
+      <div className="container">
+
+        <div className="row">
+          {classList}
+        </div>
+      </div>
     </div>
   )
 }
